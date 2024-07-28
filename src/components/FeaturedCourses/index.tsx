35c337279fe2fc85fbb6352ba6aefbff6bbc091d
Gabriel Hamasaki <gabriel.hamasaki@exata.it>
2024-07-28 13:57:09 -0300

    fix: no reload on index

--- src/components/FeaturedCourses/index.tsx
+++ src/components/FeaturedCourses/index.tsx
@@ -1,11 +1,12 @@
 import type { Course } from "@/types/courses";
 import { Card, CardContent, CardTitle } from "../ui/card";
 
-type FeaturedCoursesProps = {
-  courses: Course[];
-}
+const response = await fetch(import.meta.env.PUBLIC_API_URL + "/courses");
 
-export default function FeaturedCourses({ courses }: FeaturedCoursesProps) {
+const data = await response.json();
+const courses: Course[] = data.courses;
+
+export default function FeaturedCourses() {
   return (
     <section className="w-full py-12 md:py-24 lg:py-32">
       <div className="container px-4 md:px-6">
