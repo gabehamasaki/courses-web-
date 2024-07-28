fa6788da31083f6c64dec5fc134e6690eeea1b36
Gabriel Hamasaki <gabriel.hamasaki@exata.it>
2024-07-28 14:34:06 -0300

    feat: add carousel

--- src/components/FeaturedCourses/index.tsx
+++ src/components/FeaturedCourses/index.tsx
@@ -1,5 +1,14 @@
 import type { Course } from "@/types/courses";
 import { Card, CardContent, CardTitle } from "../ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+  type CarouselApi,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay"
 
 const response = await fetch(import.meta.env.PUBLIC_API_URL + "/courses");
 
@@ -7,6 +16,7 @@ const data = await response.json();
 const courses: Course[] = data.courses;
 
 export default function FeaturedCourses() {
+
   return (
     <section className="w-full py-12 md:py-24 lg:py-32">
       <div className="container px-4 md:px-6">
@@ -19,29 +29,44 @@ export default function FeaturedCourses() {
             </p>
           </div>
         </div>
-        <div className="mx-auto grid gap-6 py-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:gap-8">
-          {courses.map((course) => (
-            <Card key={course.id} className="group relative overflow-hidden rounded-lg bg-background shadow-sm transition-all hover:shadow-md">
-              <img
-                src={course.image_url}
-                width="550"
-                height="310"
-                alt={course.name}
-                className="aspect-video w-full object-cover object-center"
-              />
-              <CardContent className="p-4">
-                <div className="flex flex-col">
-                  <h3 className="text-lg font-semibold group-hover:underline text-primary">
-                    {course.name}
-                  </h3>
-                  <h5 className="text-muted-foreground/80 text-xs">By {course.teacher.name}</h5>
-                </div>
-                <p className="mt-2 text-sm text-muted-foreground line-clamp-2">
-                  {course.description}
-                </p>
-              </CardContent>
-            </Card>
-          ))}
+        <div className="mx-auto w-full mt-8">
+          <Carousel
+            plugins={[
+              Autoplay({
+                delay: 2000,
+              }),
+            ]}
+          >
+            <CarouselContent>
+              {courses.map((course) => (
+                <CarouselItem key={course.id} className="sm:basis-1/2 md:basis-1/3">
+                  <Card className="group relative overflow-hidden  rounded-lg bg-background shadow-sm transition-all hover:shadow-md">
+                    <img
+                      src={course.image_url}
+                      width="550"
+                      height="310"
+                      alt={course.name}
+                      className="aspect-video w-full object-cover object-center"
+                    />
+                    <CardContent className="p-4">
+                      <div className="flex flex-col">
+                        <h3 className="text-lg font-semibold group-hover:underline text-primary">
+                          {course.name}
+                        </h3>
+                        <h5 className="text-muted-foreground/80 text-xs">By {course.teacher.name}</h5>
+                      </div>
+                      <p className="mt-2 text-sm text-muted-foreground line-clamp-2">
+                        {course.description}
+                      </p>
+                    </CardContent>
+                  </Card>
+                </CarouselItem>
+              ))}
+
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
         </div>
       </div>
     </section>
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,4 +1,5 @@
 export default function HeroSection() {
+
   return (
     <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
       <div className="container px-4 md:px-6">
